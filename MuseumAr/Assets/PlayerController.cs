3558b583d36893380e4fc60a4840a1f5492192a6
Temperiest <43546940+Temperiest@users.noreply.github.com>
2020-07-08 19:16:45 -0400

    Functional Prototipe

---
+++ MuseumAr/Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject player;
+    public Rigidbody2D playerRig2D;
+    public float speed;
+    public float maxRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if (x < 0)
+        {
+            MoveLeft();
+        }
+        if(x > 0)
+        {
+            MoveRight();
+        }
+        if(x == 0)
+        {
+            Stop();
+        }
+
+    }
+
+    void MoveRight()
+    { 
+        playerRig2D.velocity = new Vector2(speed,0);
+    }
+
+    void MoveLeft()
+    {
+        playerRig2D.velocity = new Vector2(-speed, 0);
+
+    }
+
+    void Stop()
+    {
+        playerRig2D.velocity = Vector2.zero;
+    }
+}

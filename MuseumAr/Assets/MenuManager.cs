b488a309675856fb77004fc6e9fa67a68125aae9
Temperiest <43546940+Temperiest@users.noreply.github.com>
2020-07-08 01:50:10 -0400

    MenuSistemScenes

---
+++ MuseumAr/Assets/MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Canvas maincanvas;
+    public GameObject mainMenu;
+    public GameObject selecctionMenu;
+    public GameObject MuseumMenu;
+    public GameObject QRmenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMenuActive(GameObject gameObject)
+    {
+        SetMenuOff();
+        gameObject.SetActive(true);
+    }
+
+    public void SetMenuOff()
+    {
+        mainMenu.SetActive(false);
+        selecctionMenu.SetActive(false);
+        MuseumMenu.SetActive(false);
+        QRmenu.SetActive(false);
+    }
+
+    public void Loadlevel(string level)
+    {
+        SceneManager.LoadScene(level);
+
+    }
+
+}

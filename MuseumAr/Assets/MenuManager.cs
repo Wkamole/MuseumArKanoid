ec300f63f44bf5b13351c7fd2b0d70e8fb081538
Temperiest <43546940+Temperiest@users.noreply.github.com>
2020-08-11 03:26:32 -0400

    dddd

--- MuseumAr/Assets/MenuManager.cs
+++ MuseumAr/Assets/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HUDManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     public Canvas maincanvas;
     public GameObject mainMenu;

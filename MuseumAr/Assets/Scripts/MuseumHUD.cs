9753d0ebf6f4a395dfa679ae87c4df3b922eaa32
Temperiest <43546940+Temperiest@users.noreply.github.com>
2020-08-11 04:14:27 -0400

    Menus

--- MuseumAr/Assets/Scripts/MuseumHUD.cs
+++ MuseumAr/Assets/Scripts/MuseumHUD.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MuseumHUD : MonoBehaviour
+public class MuseumHUD : HUDManager
 {
     // Start is called before the first frame update
     void Start()
--- MuseumAr/Assets/Scripts/OptionHUD.cs
+++ MuseumAr/Assets/Scripts/OptionHUD.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OptionHUD : MonoBehaviour
+public class OptionHUD : HUDManager
 {
     // Start is called before the first frame update
     void Start()

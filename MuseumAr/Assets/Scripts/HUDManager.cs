56d34a01faafd8d8d9cb3aca9e8421866e497f81
Temperiest <43546940+Temperiest@users.noreply.github.com>
2020-08-11 03:36:56 -0400

    Revert "dddd"
    This reverts commit ec300f63f44bf5b13351c7fd2b0d70e8fb081538.

--- MuseumAr/Assets/Scripts/HUDManager.cs
+++ MuseumAr/Assets/Scripts/HUDManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class HUDManager : MonoBehaviour
 {
     public Canvas maincanvas;
     public GameObject mainMenu;

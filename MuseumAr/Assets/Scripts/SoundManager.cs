8fc7e17e4dd115b663c0a3341fdb9b7552d8e093
Temperiest <43546940+Temperiest@users.noreply.github.com>
2020-08-11 16:29:25 -0400

    Audio Sistem

---
+++ MuseumAr/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    public static SoundManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds )
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("GameMusic");
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            return;
+        }
+
+        s.source.Play();
+    }
+
+
+
+
+
+
+}

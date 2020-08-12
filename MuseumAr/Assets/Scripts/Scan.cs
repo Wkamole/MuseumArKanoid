5779910e388af811a7330a602808693969e97701
ismaelKeupuchur <64103919+ismaelKeupuchur@users.noreply.github.com>
2020-08-12 04:27:04 -0400

    Cambios Escena QR
    Agregado Paquete Vuforia y configuracion de Build a Android. Añadido script y modificado escena QR

---
+++ MuseumAr/Assets/Scripts/Scan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class Scan : MonoBehaviour
+{
+    GameObject activate;
+    
+
+    void Start()
+    {
+        activate = GameObject.Find("Load1");
+
+
+        if (activate.active)
+        {
+            Debug.Log("Activado");
+            MenuManager.instace.unlock = true;
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+}
